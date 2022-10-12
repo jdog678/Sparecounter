fb316fa4bb9c69f639ed00bac71463c6789b1ca8
jdog678 <106761975+jdog678@users.noreply.github.com>
2022-10-12 14:10:18 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// set inital value to zero
+let count = 0;
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else if (styles.contains("increasetwo")){
+        count++ + count++;
+    }else if (styles.contains("decreasetwo")){
+        count-- - count--;
+    }else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    if (count === 100) {
+        value.style.color = "#FFC0CB";
+    }
+    value.textContent = count;
+  });
+});
